4e575fa06220020d6bcc22a580bb6a0e210adbbb
Mohit Agrawal <mohit2005agrawal@gmail.com>
2026-07-15 01:29:36 +0530

    learn about salt

--- backend/day18/hashing.js
+++ backend/day18/hashing.js
@@ -1,9 +1,15 @@
 const bcrypt = require('bcrypt')
-
 async function main(){
     const password = "mohit123"
-    const hashpassword = await bcrypt.hash(password,10)
+    console.time("hash")
+    // const hashpassword = await bcrypt.hash(password,10)\
+// or
+    const salt = await bcrypt.genSalt(10)
+    const hashpassword = await bcrypt.hash(password,salt)
+
+    console.timeEnd("hash")
+
+    console.log(salt)
     console.log(hashpassword)
 }
-
 main()
